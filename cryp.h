f33d965d32e5188a6bebe97865cb8f45c7a5585d
LtLollo <ltlollo.it@gmail.com>
2018-10-07 19:10:18 +0200

    comment licence

--- cryp.h
+++ cryp.h
@@ -1,3 +1,6 @@
+// This is free and unencumbered software released into the public domain.
+// For more information, see LICENS
+
 #include <immintrin.h>
 
 __m128i mul_gf2e128mod0x87(__m128i x, __m128i h);
